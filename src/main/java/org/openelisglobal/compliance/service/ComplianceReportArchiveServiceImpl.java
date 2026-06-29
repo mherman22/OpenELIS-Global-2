406dd48c2ee4fed9160541c6d90f1c28c1dce83a
reagan-meant <reaganmeant@gmail.com>
2026-06-29 19:32:27 +0300

    refactor: remove unused import for StandardCharsets in ComplianceReportArchiveServiceImpl

--- src/main/java/org/openelisglobal/compliance/service/ComplianceReportArchiveServiceImpl.java
+++ src/main/java/org/openelisglobal/compliance/service/ComplianceReportArchiveServiceImpl.java
@@ -1,6 +1,5 @@
 package org.openelisglobal.compliance.service;
 
-import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.time.OffsetDateTime;
